4066935602060bde41d3ff04e453ec2ddb559074
vitozs <94878000+vitozs@users.noreply.github.com>
2023-02-07 12:44:26 -0300

    Configurando o menu responsivo

--- scripts/menu.js
+++ scripts/menu.js
@@ -17,3 +17,41 @@ function scroll(){
 
     console.log(scrolly)
 }
+
+
+
+document.getElementById('icon').addEventListener('click', ()=>{
+    let menu = document.querySelector('#menu')
+    let icon = document.querySelector('.icon')
+    let nav = document.querySelector('#mobile ul')
+    let redes = document.querySelector('#mobile div.redes')
+    let links = document.querySelectorAll('#mobile a')
+    
+    for(let i=0; i<links.length; i++){
+        links[i].addEventListener('click', ()=>{
+            menu.classList.remove('toggle-menu')
+            nav.classList.add('d-none');
+            nav.classList.remove('d-flex');
+            redes.classList.add('d-none');
+            redes.classList.remove('d-flex');
+        })
+    }
+    if(!menu.classList.contains('toggle-menu')){
+        menu.classList.add('toggle-menu')   ;     
+        nav.classList.remove('d-none');
+        nav.classList.add('d-flex');
+        redes.classList.remove('d-none');
+        redes.classList.add('d-flex');
+
+
+
+
+
+    }else{
+        menu.classList.remove('toggle-menu')
+        nav.classList.add('d-none');
+        nav.classList.remove('d-flex');
+        redes.classList.add('d-none');
+        redes.classList.remove('d-flex');
+    }       
+})
